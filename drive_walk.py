028f304553cc2862731e25a5ce91a9f944b4670a
labroid <orders@jacksonhome.net>
2018-03-28 21:09:57 -0400

    Non-working copy before refactor to umongo and gphoto objects

--- drive_walk.py
+++ drive_walk.py
@@ -1,5 +1,6 @@
 import mongoengine as me
 import time
+import functools
 import os
 from apiclient.discovery import build  # pip install google-api-python-client
 import logging
@@ -13,10 +14,9 @@ FILE_FIELDS = "id,imageMediaMetadata/time,md5Checksum,mimeType,name,originalFile
 INIT_FIELDS = f"files({FILE_FIELDS}), nextPageToken"
 UPDATE_FIELDS = f"changes(file({FILE_FIELDS}),fileId,removed),nextPageToken"
 
+service = None
 cfg = Config()
 me.connect(db=cfg.gphotos.database, host=cfg.gphotos.host, alias=cfg.gphotos.gphoto_db_alias)
-service = None
-
 
 def main():
     global service
@@ -41,18 +41,18 @@ class GphotoSync:
     def __init__(self):
         dictConfig(cfg.logging)
         self.log = logging.getLogger(__name__)
-        self.root = None
+        self.root = self.get_google_photos_root()
 
     def sync(self, rebuild=False):
         if rebuild:
             self.rebuild_db()
             return
+        # TODO:  Why .first() instead of find_one? Or some other way to assure single instance?
         change_query = Gphoto_change.objects(type='change_start_page_token').first()
-        if change_query is not None:
-            change_token = change_query.value
-            self.get_changes(change_token)
-        else:
+        if change_query is None:
             self.rebuild_db()
+        else:
+            self.update_db(change_query.value)
         self.update_start_token()
         # TODO: We need some way of recording success. And do we allow restarts or always purge?  Dirty/Clean in Gphoto_change?
 
@@ -60,12 +60,7 @@ class GphotoSync:
         start_time = time.time()
         Gphoto.drop_collection()
         Gphoto_change.drop_collection()
-        gphotos = service.files().list(q="name = 'Google Photos' and trashed = false").execute()
-        num_files = len(gphotos['files'])
-        assert num_files == 1, f"Got {num_files} Google Photos nodes"
-        gphoto_node = self.steralize(gphotos['files'][0])
-        self.root = gphoto_node['gid']
-        self.walk(parent=self.steralize(gphoto_node))
+        self.walk(parent=self.root)
         self.update_start_token()
         print(f"Elapsed time: {time.time() - start_time}")
 
@@ -74,86 +69,106 @@ class GphotoSync:
         folders = []
         db_nodes = []
         path.append(parent['name'])
-        print("Path: {}".format(path)) # TODO: Change to log
+        self.log.info(f"Path: {path}")
         for node in self.get_nodes(parent):
             node['path'] = path
-            clean_node = self.steralize(node)
             if node['mimeType'] == FOLDER:
-                folders.append(clean_node)
-            db_nodes.append(Gphoto(**clean_node))
+                folders.append(node)
+            db_nodes.append(Gphoto(**node))
         Gphoto.objects.insert(db_nodes)
         for parent in folders:
             self.walk(parent, path)
         path.pop()
 
-    def steralize(self, node):
-        if 'id' in node:  # Mongoengine reserves 'id'
-            node['gid'] = node.pop('id')
-        if 'size' in node:  # Mongoengine reserves 'size'
-            node['gsize'] = node.pop('size')
-        return node
-
     def get_nodes(self, parent):
         nodes = []
         nextpagetoken = None
         while True:
-            response = service.files().list(q=f"'{parent['gid']}' in parents and trashed = false",
+            #TODO:  Consider gzip and batch requests
+            #TODO:  Add error trapping on google returns
+            response = service.files().list(q=f"'{parent['id']}' in parents and trashed = false",
                                             pageSize=1000,
                                             pageToken=nextpagetoken,
                                             fields=INIT_FIELDS).execute()
-            print(f"Drive delivered {len(response['files'])} files")
+            self.log.info(f"Drive delivered {len(response['files'])} files")
             nodes.extend(response['files'])
             nextpagetoken = response.get('nextPageToken')
             if nextpagetoken is None:
                 return nodes
 
-    def get_node_by_name(self, name):
-        gphotos = service.files().list(q=f"name = '{name}' and trashed = false").execute()
-        return self.steralize(gphotos['files'][0])
+    def get_google_photos_root(self):
+        gphotos = service.files().list(q="name = 'Google Photos' and trashed = false").execute()
+        num_files = len(gphotos['files'])
+        assert num_files == 1, f"Got {num_files} Google Photos nodes. Should be 1"
+        return gphotos['files'][0]
 
     def update_start_token(self):
         start_token = service.changes().getStartPageToken().execute()
         Gphoto_change.objects(type='change_start_page_token').modify(upsert=True, value=start_token['startPageToken'])
 
-    def get_changes(self, change_token):
+    def update_db(self, change_token):
         delete_count = new_count = 0
         while True:
             response = service.changes().list(pageToken=change_token,
                                               pageSize=1000,
                                               includeRemoved=True,
                                               fields=UPDATE_FIELDS).execute()
-
             change_count = len(response.get('changes', []))
             self.log.info("Google sent {} change records".format(change_count))
-            if change_count:
-                for change in response['changes']:
-                    if change['removed'] is True:
-                        Gphoto.objects(gid=change['fileId']).delete()
-                        delete_count += 1
-                    else:
-                        clean_change = self.steralize(change['file'])
-                        Gphoto.objects(gid=clean_change['gid']).update_one(upsert=True, **clean_change)
-                        new_count += 1
+            for change in response.get('changes'):
+                if change['removed'] or change['file']['trashed']:
+                    print("Delete file here")
+                    # Gphoto.objects(id=change['fileId']).delete()
+                    delete_count += 1
+                else:
+                    #TODO: Clear path as node may have moved..
+                    print(f"update file {change['name']}")
+                    # Gphoto.objects(id=change['file']['id']).update_one(upsert=True, **change)
+                    new_count += 1
             change_token = response.get('nextPageToken')
             if change_token is None:
                 break
         self.set_paths()
-        self.log.info(f"Sync update complete.  New files: {new_count} Deleted files: {delete_count}")
+        self.log.info(f"Sync update complete. New files: {new_count} Deleted files: {delete_count}")
 
     def set_paths(self):
-        orphans = Gphoto.objects(path=None)
+        orphans = Gphoto.objects(path=None) #TODO: Also check path=[]?
         for orphan in orphans:
-            path = self.ascend(orphan['id'])
-            Gphoto.objects(gid=orphan['gid']).update_one(upsert=True, path=path)
+            path = self.get_node_path(orphan) # TODO: Should be whatever recursive calls get_node_path()
+            Gphoto.objects(id=orphan['id']).update_one(upsert=True, path=path)
+        self.log.info(f"Cache stats: {self.get_node_path.cache_info()}")
+
+    @functools.lru_cache()
+    def get_node_path(self, node):
+        if node.id == self.root['id']:
+            return []
+        parent = Gphoto.objects(id=node.parents[0]).first() #TODO: Am using first(). This might be better as find_one or other unique find
+        if parent is not None:
+            path = parent.get('path')
+            if path is not None:
+                return path
+            #Fix starting here
+        parent = self.get_node_from_id(node['parent'][0])
+        return self.get_node_path(parent['parent'][0]) + parent[node['parent'][0]]
+
+    def get_node_from_id(self, node_id):
+        gphotos = service.files().list(q=f"id = '{node_id}' and trashed = false").execute()
+        # TODO:  Add error checking?
+        return gphotos['files'][0] #Fails on no 'files' key
+
 
     def ascend(self, node):
-        parent = Gphoto.objects(gid=node['parents'][0]).get()
-        if parent['gid'] == self.root:
+        parent = Gphoto.objects(id=node.parents[0])
+        # assert parent.count() == 1, "Ascend: More than one file with same id returned"
+        if parent is None:
+            pass
+            # TODO:  Hmmmm....maybe parent isn't yet in database. Need to scan rest of changes for the parent.
+        if parent.id == self.root['id']:
             return ['Google Photos']
-        path = parent['path']
+        path = parent.path
         if path is None:
             path.append(self.ascend(parent))
-        return path.append(parent['name'])
+        return path.append(parent.name)
 
 
 if __name__ == '__main__':
